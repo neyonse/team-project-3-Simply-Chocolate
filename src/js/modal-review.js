fffbfcbc65474b5e275652abf0dfdd507722d0f7
Artemzaicew <122361190+Artemzaicew@users.noreply.github.com>
2023-03-19 14:15:15 +0200

    add modal-review

---
+++ src/js/modal-review.js
@@ -0,0 +1,23 @@
+(() => {
+  const mobileMenu = document.querySelector('.js-review');
+  const openMenuBtn = document.querySelector('.js-open-menu-review');
+  const closeMenuBtn = document.querySelector('.js-close-menu-review');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 320px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
