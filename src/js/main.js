5c258029cb24777df696981559f596e02ffa97ab
neyonse <neyonse@gmail.com>
2023-03-19 21:58:41 +0200

    update main.js

--- src/js/main.js
+++
@@ -1,7 +0,0 @@
-import './modal-buy-now.js';
-import './slick.min.js';
-import './buy-now.js';
-import './mobile-menu.js';
-import './swiper.js';
-import './modal-review.js';
-import './modal-subscribe.js';
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './js/modal-buy-now.js';
+import './js/slick.min.js';
+import './js/buy-now.js';
+import './js/mobile-menu.js';
+import './js/swiper.js';
+import './js/modal-review.js';
+import './js/modal-subscribe.js';
