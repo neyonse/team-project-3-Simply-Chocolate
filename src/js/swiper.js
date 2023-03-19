6ca26e1aca5cc9967075c5381766398192bfb897
Artemzaicew <122361190+Artemzaicew@users.noreply.github.com>
2023-03-19 17:38:15 +0200

    bugfix swiper

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,4 +1,25 @@
 const swiper = new Swiper('.swiper', {
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      slidesPerGroup:1,
+      spaceBetween: 16,
+    },
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+      slidesPerGroup:2,
+      spaceBetween: 16
+    },
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 3,
+      slidesPerGroup:3,
+      spaceBetween: 28
+    }
+  },
+
   // Optional parameters
   direction: 'horizontal',
   loop: true,
@@ -6,7 +27,7 @@ const swiper = new Swiper('.swiper', {
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
-    clickable: true
+    clickable: true,  
   },
 
   // Navigation arrows
