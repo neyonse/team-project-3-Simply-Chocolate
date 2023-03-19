ccab79501bab07713afc20adffe21925725f5617
neyonse <neyonse@gmail.com>
2023-03-19 22:38:52 +0200

    test 4

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import './js/modal-buy-now.js';
-import './js/slick.min.js';
-import './js/buy-now.js';
-import './js/mobile-menu.js';
-import './js/swiper.js';
-import './js/modal-review.js';
-import './js/modal-subscribe.js';
+import './js/slick.min';
+import './js/modal-buy-now';
+import './js/buy-now';
+import './js/mobile-menu';
+import './js/swiper';
+import './js/modal-review';
+import './js/modal-subscribe';
+// import './js/modal';
