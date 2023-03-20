7358571ec7187ac2ebaccff193b01f01ecd0414a
neyonse <neyonse@gmail.com>
2023-03-20 14:35:35 +0200

    test

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './js/buy-now';
+import './js/modal-buy-now.js';
 import './js/modal-review';
-// import './js/modal-buy-now.js';
 import './js/mobile-menu.js';
 // import './js/how-its-made.js';
 // import './js/modal-subscribe.js';
