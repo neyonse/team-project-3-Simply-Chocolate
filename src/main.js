42754c936b49e28a851dee4ae92995bd010c51a7
neyonse <neyonse@gmail.com>
2023-03-20 17:30:09 +0200

    small update

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 // import './js/modal-buy-now';
 // import './js/slick.min';
 
+import './js/mobile-menu';
 import './js/buy-now';
 import './js/modal-review';
-import './js/mobile-menu';
 import './js/modal-subscribe';
 import './js/modal-thanks';
 import './js/modal-feedback';
