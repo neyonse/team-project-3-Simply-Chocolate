c8f6b4d553cc92106cc71038948474ef6ac5c719
neyonse <neyonse@gmail.com>
2023-03-20 15:34:45 +0200

    test

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import './js/buy-now';
 import './js/modal-review';
 import './js/mobile-menu';
+// import './js/how-its-made.js';
 import './js/modal-subscribe';
 import './js/modal-thanks';
 import './js/modal-feedback';
-// import './js/how-its-made.js';
-import './js/swiper';
-import './js/slick.min';
-// import './js/modal-buy-now';
+import './js/swiper.js';
+import './js/slick.min.js';
+import './js/modal-buy-now.js';
