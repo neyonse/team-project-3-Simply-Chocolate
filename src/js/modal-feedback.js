8e51adab4312a9fc9a03627ec80ad1c62c286c8a
neyonse <neyonse@gmail.com>
2023-03-20 19:19:23 +0200

    add scroll lock

--- src/js/modal-feedback.js
+++ src/js/modal-feedback.js
@@ -8,7 +8,6 @@
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
-    document.body.classList.toggle('no-scroll');
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
