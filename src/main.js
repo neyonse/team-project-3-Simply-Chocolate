4e8438a0e9c4b78090ac83e0b13a11c7c775a728
neyonse <neyonse@gmail.com>
2023-03-20 13:18:51 +0200

    test

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './js/slick.min.js';
 import './js/swiper.js';
 import './js/buy-now.js';
-import './js/modal-buy-now.js';
+// import './js/modal-buy-now.js';
 // import './js/mobile-menu.js';
 // import './js/how-its-made.js';
 // import './js/modal-review.js';
