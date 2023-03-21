63fc9fb544e5cf386a52c2273d9daa27f6710b4b
neyonse <neyonse@gmail.com>
2023-03-21 17:35:20 +0200

    some fixes and remove unnessesary files

--- src/main.js
+++ src/main.js
@@ -5,13 +5,8 @@ import './js/modal-subscribe';
 import './js/modal-thanks';
 import './js/modal-feedback';
 
-// import './js/swiper';
-// import './js/swiper-products';
-
-import './js/modal-buy-now';
-
 import './js/slick-slider-buy-now';
 import './js/slick-slider-our-products';
 import './js/slick-slider-reviews';
 
-// import './js/how-its-made';
+import './js/how-its-made';
