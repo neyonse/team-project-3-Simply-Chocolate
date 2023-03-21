7ef23c8047a54f4af37940076ba07fb09036e5f4
neyonse <neyonse@gmail.com>
2023-03-21 17:17:07 +0200

    add our-products

--- src/js/slick-slider-our-products.js
+++ src/js/slick-slider-our-products.js
@@ -1,27 +1,9 @@
 $(document).ready(function () {
-  var productsSlider = $('.our-products-slider');
-
-  function disableSlider() {
-    if ($(window).width() < 1200) {
-      if (!productsSlider.hasClass('slick-initialized')) {
-        productsSlider.slick({
-          arrows: false,
-          dots: true,
-          speed: 1000,
-          variableWidth: true,
-          waitForAnimate: false,
-        });
-      }
-    } else {
-      if (productsSlider.hasClass('slick-initialized')) {
-        productsSlider.slick('unslick');
-      }
-    }
-  }
-
-  disableSlider();
-
-  $(window).on('resize', function () {
-    disableSlider();
+  $('.our-products-slider').slick({
+    arrows: false,
+    dots: true,
+    variableWidth: true,
+    speed: 1000,
+    infinite: false,
   });
 });
